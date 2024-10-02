8ca2ada99076e12ef039175038d1b8c0ec92ee22
Diego Anderson <diego.fernandes@servicenet.com.br>
2024-10-02 16:55:33 -0300

    ajuste no gitignore

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data.categorias import categorias
 
 def main():
     # Cód Bancos: Bradesco(237), Nubank(260), XP(348)
-    process_csv('nubank.csv', categorias, 260)
+    process_csv('nubank-2024-09.csv', categorias, 260)
     
 if __name__ == "__main__":
     main()
